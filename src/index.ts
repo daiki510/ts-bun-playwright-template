ac9b3e42f78d9d78d9e1ccbf3d822654cc82b467
daiki510 <ddgg7755@gmail.com>
2024-04-06 21:59:40 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-const server = Bun.serve({
-  port: 3000,
-  fetch(req) {
-    return new Response("Bun!");
-  },
-});
+import { chromium } from "playwright-core";
 
-console.log(`Listening on http://localhost:${server.port} ..`);
+(async () => {
+  const browser = await chromium.launch({
+    channel: "chrome",
+    headless: false,
+  });
+  const page = await browser.newPage();
+  await page.goto("https://www.google.co.jp/");
+  await page.screenshot({ path: `tmp/sample.png` });
+  await browser.close();
+})();
